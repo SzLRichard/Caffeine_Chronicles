ba0de15e2b8e7614ca6868da87d6403423a47568
SzLRichard <szrichard2004@gmail.com>
2024-07-11 19:54:36 +0300

    Fixed volume slider synchronization

--- Assets/Scripts/settingsMenu.cs
+++ Assets/Scripts/settingsMenu.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 public class settingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider volumeSlider;
+    private void Start()
+    {
+        float volumeLevel;
+        audioMixer.GetFloat("volume",out volumeLevel);
+        volumeSlider.value = volumeLevel;
+    }
     public void SetVolume(float volume) {
         audioMixer.SetFloat("volume", volume);
     }
